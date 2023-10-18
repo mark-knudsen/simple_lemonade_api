4aab09bf4b94aa78cf3d78c441bc90f6c140a2f6
mark-knudsen <marksvendstrupknudsen@gmail.com>
2023-10-18 22:37:32 +0200

    small change

--- server.js
+++ server.js
@@ -100,9 +100,7 @@ const corsOptions = {
   
 };
 
-//app.use(cors(corsOptions));
-
-
+app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
